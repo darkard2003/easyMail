7808571d5cc483727fd9955ad818b093f30d2797
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2023-11-22 23:54:54 +0530

    easyMail: fixed html support

--- easyMail_test.go
+++ easyMail_test.go
@@ -15,8 +15,8 @@ func TestSendMail(t *testing.T) {
 
 	t.Logf("Email: %s", email)
 
-	if email == "" || password == "" || recivers == "" {
-		t.Error("Please set TEST_EMAIL and TEST_PASSWORD and TEST_EMAIL_RECIVER environment variables to run this test")
+	if email == "" || password == "" {
+		t.Error("Please set TEST_EMAIL and TEST_PASSWORD environment variables to run this test")
 	}
 
 	mserver := NewMailServer(email, password, GMAIL)
@@ -24,9 +24,39 @@ func TestSendMail(t *testing.T) {
 	mail := NewMail(
 		email,
 		reciversList,
-		"Test Subject",
-		"Test Body",
+		"Normal Test",
+		"Noice to meet ya",
 		false,
+	)
+
+	err := mserver.SendMail(mail)
+
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestSendMailWithHtml(t *testing.T) {
+	email := os.Getenv("TEST_EMAIL")
+	password := os.Getenv("TEST_EMAIL_PASSWORD")
+	recivers := os.Getenv("TEST_EMAIL_RECIVERS")
+
+	reciversList := strings.Split(recivers, ",")
+
+	t.Logf("Email: %s", email)
+
+	if email == "" || password == "" {
+		t.Error("Please set TEST_EMAIL and TEST_PASSWORD environment variables to run this test")
+	}
+
+	mserver := NewMailServer(email, password, GMAIL)
+
+	mail := NewMail(
+		email,
+		reciversList,
+		"Html Test",
+		"<h1>Hello</h1><p>Noice to meet ya</p>",
+		true,
 	)
 
 	err := mserver.SendMail(mail)
--- mail.go
+++ mail.go
@@ -35,39 +35,20 @@ func (m *Mail) AddHeader(key string, value string) {
 	m.Headers[key] = value
 }
 
-func (m *Mail) GetFormattedMails() []string {
-	mails := []string{}
-
-	var contentType string
-	if m.IsHtml {
-		contentType = "text/html; charset=UTF-8"
-	} else {
-		contentType = "text/plain; charset=UTF-8"
-	}
-
-	for _, reciver := range m.To {
-		msg := "From: " + m.From + "\n" +
-			"To: " + reciver + "\n" +
-			"Subject: " + m.Subject + "\n" +
-			contentType + "\n" +
-			"\n" +
-			m.Body +
-			"\n" +
-			"\n" +
-			"."
-		mails = append(mails, msg)
-	}
-
-	return mails
-}
-
 func (m *Mail) ToRFC822() string {
-	headers := make([]string, 0, len(m.Headers)+3)
+	headers := make([]string, 0, len(m.Headers)+4)
 
 	// Add common headers
 	headers = append(headers, fmt.Sprintf("From: %s", m.From))
 	headers = append(headers, fmt.Sprintf("To: %s", strings.Join(m.To, ", ")))
 	headers = append(headers, fmt.Sprintf("Subject: %s", m.Subject))
+
+	// Add Content-Type header based on IsHtml flag
+	if m.IsHtml {
+		headers = append(headers, "Content-Type: text/html; charset=\"UTF-8\"")
+	} else {
+		headers = append(headers, "Content-Type: text/plain; charset=\"UTF-8\"")
+	}
 
 	// Add custom headers
 	for k, v := range m.Headers {
